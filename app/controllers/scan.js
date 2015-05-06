7370361b0dfe23fd96cfe38a186e84e0187cb9fb
Allen Yao <alleyao@gmail.com>
2015-05-06 02:03:21 +0000

    Added scan controller logic

--- app/controllers/scan.js
+++ app/controllers/scan.js
@@ -2,22 +2,49 @@ var models = require('./../models');
 var Scan = models.Scan;
 
 
-exports.getScans = function() {
-
+exports.getScans = function(callback) {
+    Scan
+        .find()
+        .exec(function(err, scans) {
+            callback(err, scans);
+        });
 };
 
-exports.createScan = function() {
-
+exports.createScan = function(data, callback) {
+    var newScan = new Scan({
+        md5: data.md5,
+        results: []
+    });
+    newScan.save(function(err, scan) {
+        callback(err, scan);
+    });
 };
 
-exports.getScan = function() {
-
+exports.getScan = function(id, callback) {
+    Scan
+        .findById(id)
+        .exec(function(err, scan) {
+            callback(err, scan);
+        });
 };
 
-exports.updateScan = function() {
-
+exports.updateScan = function(id, data, callback) {
+    Scan
+        .findById(id)
+        .update({
+            md5: data.md5,
+            results: data.results
+        })
+        .exec(function(err, scan) {
+            callback(err, scan);
+        });
 };
 
-exports.deleteScan = function() {
-
+exports.deleteScan = function(id, callback) {
+    Scan
+        .findById(id)
+        .remove()
+        .exec(function(err, scan) {
+            callback(err, scan);
+        });
 };
